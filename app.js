c3a6dc91115b4f6a240dea5091e0e0a2c157bb45
Naman <namanwinner5@gmail.com>
2023-03-27 12:03:59 +0530

    first commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const server = http.createServer((req,resp)=>{
     
     console.log(req.url,req.method,req.headers);
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
         resp.write('<html>');
         resp.write('<head><title>Enter the message</title></head>')
@@ -13,7 +14,17 @@ const server = http.createServer((req,resp)=>{
         return resp.end();
     }
     if(url === '/message'){
-        fs.writeFileSync('message.txt','dummy');
+        const body = [];
+        req.on('data',(chunck)=>{
+            console.log(chunck);
+            body.push(chunck);
+        });
+        req.on('end',()=>{
+            const ParsedBody = Buffer.concat(body).toString();
+            const message = ParsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message.replaceAll('+',' '));
+        });
+       
         resp.statusCode = 302
         resp.setHeader('Location','/');
         return resp.end();
