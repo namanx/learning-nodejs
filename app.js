f1620ece40ea720c5059f140da2be7faf72338f5
Naman <namanwinner5@gmail.com>
2023-04-03 19:13:49 +0530

    In this we have added routes.js to help us to make our codeclean

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
 const http = require('http');
 const fs = require('fs');
-
-const server = http.createServer((req,resp)=>{
-    
-    console.log(req.url,req.method,req.headers);
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        resp.write('<html>');
-        resp.write('<head><title>Enter the message</title></head>')
-        resp.write('<body><form action="/message" method = "POST"><input type="text" name = "message"><button type="submit">Send</button></input></form></body>')
-        resp.write('</html>');
-        return resp.end();
-    }
-    if(url === '/message'){
-        const body = [];
-        req.on('data',(chunck)=>{
-            console.log(chunck);
-            body.push(chunck);
-        });
-        req.on('end',()=>{
-            const ParsedBody = Buffer.concat(body).toString();
-            const message = ParsedBody.split('=')[1];
-            fs.writeFileSync('message.txt',message.replaceAll('+',' '));
-        });
-       
-        resp.statusCode = 302
-        resp.setHeader('Location','/');
-        return resp.end();
-    }
-    // responses
-    resp.setHeader('Content-Type','text/html')
-    resp.write('<html>')
-    resp.write('<head><title>My first node Application</title></head>')
-    resp.write('<body>Hello folks ! I am learning node js and this time I am going to do really well</body>')
-    resp.write('</html>')
-    resp.end();
-})
-
+const routes = require('./routes');
+const server = http.createServer(routes);
 server.listen(4000)
