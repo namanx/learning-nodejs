b71b8d5406ec74c6a9007c9277768f7e2198ee8f
Naman <namanwinner5@gmail.com>
2023-04-12 17:59:32 +0530

    Here we complete our discussion on pug

--- routes/admin.js
+++ routes/admin.js
@@ -11,12 +11,13 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product');
+  res.render('add-product',{pageTitle: 'Add Product', path :'/admin/add-product'});
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
+  console.log(products)
   res.redirect('/');
 });
 
