cb8b47ef34369c57fdada091a371781e62c3ec7d
Naman <namanwinner5@gmail.com>
2023-04-18 00:29:29 +0530

    added some more code

--- controllers/products.js
+++ controllers/products.js
@@ -1,29 +1,30 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product',{pageTitle: 'Add Product',
-     path :'/admin/add-product',
-     activeAddProduct : true,
-     formsCSS : true,
-     productCSS : true
-    });
-  };
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
-    product.save();
-    console.log(product)
-    res.redirect('/');
-  };
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
 
 exports.getShopmethod = (req, res, next) => {
-  const products = Product.fetchAll();
-    res.render('shop',{prods : products,
-      pageTitle : "Shop",
-      path : '/',
-      hasProduct : products.length > 0,
-      activeShop : true,
-      productCSS : true
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
     });
-  };
-
+  });
+};
